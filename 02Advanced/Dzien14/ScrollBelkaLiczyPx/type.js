a18347efe025d6eda84b98fad06d9c074ae3fbfb
Computer Home <dariuszwr@gmail.com>
2020-10-01 21:56:38 +0200

    Add day 14 lesson scrollPx

---
+++ 02Advanced/Dzien14/ScrollBelkaLiczyPx/type.js
@@ -0,0 +1,20 @@
+$('nav li').on('click', function () {
+    const goToSection = '[data-section=' + $(this).attr('class') + ']';
+
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top - 59
+    }, 1000)
+})
+
+$(document).on('scroll', function () {
+    let scroll = $(document).scrollTop().toFixed();
+
+    $('.info').text('Obecna pozycja scrolla to: ' + $(window).scrollTop() + 'px')
+
+    if (scroll > 200) {
+        $('.info').addClass('active');
+    } else {
+        $('.info').removeClass('active');
+    }
+})
+
